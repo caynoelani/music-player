a005bfd84baa0b128fd2196227da2df29045a447
Cayla Bradley <cayla.bradley94@gmail.com>
2022-11-10 17:27:55 -0600

    menu toggle functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const btnOpenEl = document.querySelector('.btn-open');
+const btnCloseEl = document.querySelector('.btn-close');
+const mainNav = document.querySelector('.nav-main');
+const mainLogo = document.querySelector('.logo-main')
+
+btnOpenEl.addEventListener('click', () => {
+    mainNav.style.left = '0rem';
+    btnCloseEl.style.display = 'block';
+});
+
+btnCloseEl.addEventListener('click', () => {
+    mainNav.style.left = '-16rem';
+    btnCloseEl.style.display = 'none';
+});
